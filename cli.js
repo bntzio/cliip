3cd758f24acb9ec258e8635221924f460b851260
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-05-02 00:35:34 -0500

    add yargs help commands

--- cli.js
+++ cli.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 'use strict';
-const argv = require('yargs').argv;
+const yargs = require('yargs');
 
 const logger = require('./logger');
+
+const argv = yargs
+  .command('ip', 'Get your current ip')
+  .command('hostname', 'Get your current ip hostname')
+  .command('city', 'Get your current ip city')
+  .command('region', 'Get your current ip region')
+  .command('country', 'Get your current ip country')
+  .command('loc', 'Get your current ip location')
+  .command('org', 'Get current ip organization name')
+  .command('postal', 'Get current postal code for your ip organization')
+  .help()
+  .argv;
 
 function startCli(args) {
   if (args.length > 0) {
